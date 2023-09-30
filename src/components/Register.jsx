c1149d6cb15692465067b41f517711a44faac994
TahsinAhammed16 <tahsin.ahammed16@gmail.com>
2023-09-30 20:55:07 +0600

    import home, register, login from daisyUi

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,53 @@
-
 const Register = () => {
-    return (
-        <div>
-            this is register
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Register Your Account!</h1>
+            <p className="py-6">
+              Unlock a world of possibilities by creating your own account.
+              Enjoy personalized experiences, exclusive access, and seamless
+              interactions by signing up today. Join our community and take the
+              first step towards a richer online journey.
+            </p>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-success">Login</button>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Register;
